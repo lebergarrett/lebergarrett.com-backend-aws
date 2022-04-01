dfc29e0de031c16a552ea472a8323ad371ffb697
lebergarrett <lebergarrett@gmail.com>
2022-03-31 22:50:22 -0700

    Removing test file

--- test.py
+++
@@ -1,8 +0,0 @@
-site_domains = ["lebergarrett.com", "garrettleber.com"]
-site_domain = site_domains[0]
-
-for i, domain in enumerate(site_domains):
-    for subdomain in ["", "www"]:
-        record = ".".join(filter(None, [subdomain, domain]))
-        if record is not site_domain:
-            print(record, i)
